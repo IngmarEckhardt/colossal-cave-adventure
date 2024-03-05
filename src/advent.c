3505038508b301179474936c5361d9c8c0e167b6
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2024-03-05 08:41:37 +0100

    formatting

--- src/advent.c
+++ src/advent.c
@@ -6,7 +6,7 @@
 
 //#include <err.h>
 #include <stdio.h> /* drv = 1.1st file 2.def 3.A	*/
-#include <ctype.h>
+//#include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -18,8 +18,7 @@
 #define setmem(l, s, c) memset(l, c, s)
 
 //int main(int argc, char *argv[])
-int main(void)
-{
+int main(void) {
 //	int rflag; /* user restore request option	*/
 //
 //	rflag = 0;
@@ -44,23 +43,24 @@ int main(void)
 //	if (dbugflg < 2)
 //		dbugflg = 0; /* must request three times	*/
 
-	initplay();
+    initplay();
 //	if (rflag)
 //		restore();
 //	else if (yes(65, 1, 0))
-	if (yes(65, 1, 0))
-		limit = 1000;
-	else
-		limit = 330;
-	saveflg = 0;
-	srand(511); /* seed random	*/
-	while (!saveflg) {
+    if (yes(65, 1, 0)) {
+        limit = 1000;
+    } else {
+        limit = 330;
+    }
+    saveflg = 0;
+    srand(511); /* seed random	*/
+    while (!saveflg) {
         turn();
     }
 //	if (saveflg)
 //		saveadv();
 
-	return 0;
+    return 0;
 }
 
 /* ************************************************************	*/
@@ -68,87 +68,86 @@ int main(void)
 /*
 	Initialize integer arrays with sscanf
 */
-void scanint(int *pi, char *str)
-{
-	while (*str) {
-		if ((sscanf(str, "%d,", pi++)) < 1)
-			bug(41);      /* failed before EOS	*/
-		while (*str++ != ',') /* advance str pointer	*/
-			;
-	}
+void scanint(int * pi, char * str) {
+    while (*str) {
+        if ((sscanf(str, "%d,", pi++)) < 1) {
+            bug(41);
+        }      /* failed before EOS	*/
+        while (*str++ != ',') /* advance str pointer	*/
+            ;
+    }
 }
 
 /*
 	Initialization of adventure play variables
 */
-void initplay(void)
-{
-	turns = 0;
+void initplay(void) {
+    turns = 0;
 
-	/* initialize location status array */
-	setmem(cond, (sizeof(int)) * MAXLOC, 0);
-	scanint(&cond[1], "5,1,5,5,1,1,5,17,1,1,");
-	scanint(&cond[13], "32,0,0,2,0,0,64,2,");
-	scanint(&cond[21], "2,2,0,6,0,2,");
-	scanint(&cond[31], "2,2,0,0,0,0,0,4,0,2,");
-	scanint(&cond[42], "128,128,128,128,136,136,136,128,128,");
-	scanint(&cond[51], "128,128,136,128,136,0,8,0,2,");
-	scanint(&cond[79], "2,128,128,136,0,0,8,136,128,0,2,2,");
-	scanint(&cond[95], "4,0,0,0,0,1,");
-	scanint(&cond[113], "4,0,1,1,");
-	scanint(&cond[122], "8,8,8,8,8,8,8,8,8,");
+    /* initialize location status array */
+    setmem(cond, (sizeof(int)) * MAXLOC, 0);
+    scanint(&cond[1], "5,1,5,5,1,1,5,17,1,1,");
+    scanint(&cond[13], "32,0,0,2,0,0,64,2,");
+    scanint(&cond[21], "2,2,0,6,0,2,");
+    scanint(&cond[31], "2,2,0,0,0,0,0,4,0,2,");
+    scanint(&cond[42], "128,128,128,128,136,136,136,128,128,");
+    scanint(&cond[51], "128,128,136,128,136,0,8,0,2,");
+    scanint(&cond[79], "2,128,128,136,0,0,8,136,128,0,2,2,");
+    scanint(&cond[95], "4,0,0,0,0,1,");
+    scanint(&cond[113], "4,0,1,1,");
+    scanint(&cond[122], "8,8,8,8,8,8,8,8,8,");
 
-	/* initialize object locations */
-	setmem(place, (sizeof(int)) * MAXOBJ, 0);
-	scanint(&place[1], "3,3,8,10,11,0,14,13,94,96,");
-	scanint(&place[11], "19,17,101,103,0,106,0,0,3,3,");
-	scanint(&place[23], "109,25,23,111,35,0,97,");
-	scanint(&place[31], "119,117,117,0,130,0,126,140,0,96,");
-	scanint(&place[50], "18,27,28,29,30,");
-	scanint(&place[56], "92,95,97,100,101,0,119,127,130,");
+    /* initialize object locations */
+    setmem(place, (sizeof(int)) * MAXOBJ, 0);
+    scanint(&place[1], "3,3,8,10,11,0,14,13,94,96,");
+    scanint(&place[11], "19,17,101,103,0,106,0,0,3,3,");
+    scanint(&place[23], "109,25,23,111,35,0,97,");
+    scanint(&place[31], "119,117,117,0,130,0,126,140,0,96,");
+    scanint(&place[50], "18,27,28,29,30,");
+    scanint(&place[56], "92,95,97,100,101,0,119,127,130,");
 
-	/* initialize second (fixed) locations */
-	setmem(fixed, (sizeof(int)) * MAXOBJ, 0);
-	scanint(&fixed[3], "9,0,0,0,15,0,-1,");
-	scanint(&fixed[11], "-1,27,-1,0,0,0,-1,");
-	scanint(&fixed[23], "-1,-1,67,-1,110,0,-1,-1,");
-	scanint(&fixed[31], "121,122,122,0,-1,-1,-1,-1,0,-1,");
-	scanint(&fixed[62], "121,-1,");
+    /* initialize second (fixed) locations */
+    setmem(fixed, (sizeof(int)) * MAXOBJ, 0);
+    scanint(&fixed[3], "9,0,0,0,15,0,-1,");
+    scanint(&fixed[11], "-1,27,-1,0,0,0,-1,");
+    scanint(&fixed[23], "-1,-1,67,-1,110,0,-1,-1,");
+    scanint(&fixed[31], "121,122,122,0,-1,-1,-1,-1,0,-1,");
+    scanint(&fixed[62], "121,-1,");
 
-	/* initialize default verb messages */
-	scanint(actmsg, "0,24,29,0,33,0,33,38,38,42,14,");
-	scanint(&actmsg[11], "43,110,29,110,73,75,29,13,59,59,");
-	scanint(&actmsg[21], "174,109,67,13,147,155,195,146,110,13,13,");
+    /* initialize default verb messages */
+    scanint(actmsg, "0,24,29,0,33,0,33,38,38,42,14,");
+    scanint(&actmsg[11], "43,110,29,110,73,75,29,13,59,59,");
+    scanint(&actmsg[21], "174,109,67,13,147,155,195,146,110,13,13,");
 
-	/* initialize various flags and other variables */
-	setmem(visited, (sizeof(int)) * MAXLOC, 0);
-	setmem(prop, (sizeof(int)) * MAXOBJ, 0);
-	setmem(&prop[50], (sizeof(int)) * (MAXOBJ - 50), 0xff);
-	wzdark = closed = closing = holding = detail = 0;
-	limit = 100;
-	tally = 15;
-	tally2 = 0;
-	newloc = 1;
-	loc = oldloc = oldloc2 = 3;
-	knfloc = 0;
-	chloc = 114;
-	chloc2 = 140;
-	/*	dloc[DWARFMAX-1] = chloc				*/
-	scanint(dloc, "0,19,27,33,44,64,114,");
-	scanint(odloc, "0,0,0,0,0,0,0,");
-	dkill = 0;
-	scanint(dseen, "0,0,0,0,0,0,0,");
-	clock1 = 30;
-	clock2 = 50;
-	panic = 0;
-	bonus = 0;
-	numdie = 0;
-	daltloc = 18;
-	lmwarn = 0;
-	foobar = 0;
-	dflag = 0;
-	gaveup = 0;
-	saveflg = 0;
+    /* initialize various flags and other variables */
+    setmem(visited, (sizeof(int)) * MAXLOC, 0);
+    setmem(prop, (sizeof(int)) * MAXOBJ, 0);
+    setmem(&prop[50], (sizeof(int)) * (MAXOBJ - 50), 0xff);
+    wzdark = closed = closing = holding = detail = 0;
+    limit = 100;
+    tally = 15;
+    tally2 = 0;
+    newloc = 1;
+    loc = oldloc = oldloc2 = 3;
+    knfloc = 0;
+    chloc = 114;
+    chloc2 = 140;
+    /*	dloc[DWARFMAX-1] = chloc				*/
+    scanint(dloc, "0,19,27,33,44,64,114,");
+    scanint(odloc, "0,0,0,0,0,0,0,");
+    dkill = 0;
+    scanint(dseen, "0,0,0,0,0,0,0,");
+    clock1 = 30;
+    clock2 = 50;
+    panic = 0;
+    bonus = 0;
+    numdie = 0;
+    daltloc = 18;
+    lmwarn = 0;
+    foobar = 0;
+    dflag = 0;
+    gaveup = 0;
+    saveflg = 0;
 }
 
 
--- src/database.c
+++ src/database.c
@@ -22,124 +22,125 @@
 /*
 	Routine to fill travel array for a given location
 */
-void gettrav(int loc)
-{
-	char atrav[240] = { 0 }; /* max length of any cave pos = 144 bytes */
-	char *aptr;
-	long t;
-	int i;
+void gettrav(int loc) {
+    char atrav[240] = {0}; /* max length of any cave pos = 144 bytes */
+    char * aptr;
+    long t;
+    int i;
 
-	if (loc <= 0 || loc >= (int)cavesz)
-		bug(42);
-	if (strlen(cave[loc - 1]) > sizeof(atrav))
-		bug(43);
+    if (loc <= 0 || loc >= (int) cavesz) {
+        bug(42);
+    }
+    if (strlen(cave[loc - 1]) > sizeof(atrav)) {
+        bug(43);
+    }
 
-	strcpy(atrav, cave[loc - 1]);
-	while ((aptr = strrchr(atrav, ',')))
-		*aptr = '\0'; /* terminate substring	*/
-	aptr = &atrav[0];
-	for (i = 0; i < MAXTRAV - 1; ++i) {
-		t = atol(aptr); /* convert to long int	*/
-		travel[i].tcond = (int)(t % 1000L);
-		t /= 1000L;
-		travel[i].tverb = (int)(t % 1000L);
-		t /= 1000L;
-		travel[i].tdest = (int)(t % 1000L);
-		while (*(aptr++))
-			; /* to next substring	*/
-		if (!(*aptr)) {
-			travel[++i].tdest = -1; /* end of array	*/
-			if (dbugflg) {
-				for (i = 0; i < MAXTRAV; ++i) {
-					printf("cave[%d] = %d %d %d\n", loc,
-					       travel[i].tdest, travel[i].tverb, travel[i].tcond);
-				}
-			}
-			return; /* terminate for loop	*/
-		}
-	}
-	bug(33);
+    strcpy(atrav, cave[loc - 1]);
+    while ((aptr = strrchr(atrav, ','))) {
+        *aptr = '\0';
+    } /* terminate substring	*/
+    aptr = &atrav[0];
+    for (i = 0; i < MAXTRAV - 1; ++i) {
+        t = atol(aptr); /* convert to long int	*/
+        travel[i].tcond = (int) (t % 1000L);
+        t /= 1000L;
+        travel[i].tverb = (int) (t % 1000L);
+        t /= 1000L;
+        travel[i].tdest = (int) (t % 1000L);
+        while (*(aptr++)); /* to next substring	*/
+        if (!(*aptr)) {
+            travel[++i].tdest = -1; /* end of array	*/
+            if (dbugflg) {
+                for (i = 0; i < MAXTRAV; ++i) {
+                    printf("cave[%d] = %d %d %d\n", loc,
+                           travel[i].tdest, travel[i].tverb, travel[i].tcond);
+                }
+            }
+            return; /* terminate for loop	*/
+        }
+    }
+    bug(33);
 }
 
 /*
 	Routine to request a yes or no answer to a question.
 */
-int yes(int msg1, int msg2, int msg3)
-{
-	char answer[80];
+int yes(int msg1, int msg2, int msg3) {
+    char answer[80];
 
-	if (msg1)
-		rspeak(msg1);
-	fputs("> ", stdout);
-	fflush(stdout);
-	if (NULL == fgets(answer, 80, stdin))
-		exit(0);
-	if (tolower(answer[0]) == 'n') {
-		if (msg3)
-			rspeak(msg3);
-		return 0;
-	}
-	if (msg2)
-		rspeak(msg2);
-	return 1;
+    if (msg1) {
+        rspeak(msg1);
+    }
+    fputs("> ", stdout);
+    fflush(stdout);
+    if (NULL == fgets(answer, 80, stdin)) {
+        exit(0);
+    }
+    if (tolower(answer[0]) == 'n') {
+        if (msg3) {
+            rspeak(msg3);
+        }
+        return 0;
+    }
+    if (msg2) {
+        rspeak(msg2);
+    }
+    return 1;
 }
 
 /*
 	Print a location description from "advent4.txt"
 */
-void rspeak(int msg)
-{
+void rspeak(int msg) {
 
-	fputs(adventtxt4[msg - 1], stdout);
+    fputs(adventtxt4[msg - 1], stdout);
 
-	return;
+    return;
 }
 
 /*
 	Print an item message for a given state from "advent3.txt"
 */
-void pspeak(int item, int state)
-{
-	const char *p;
+void pspeak(int item, int state) {
+    const char * p;
 
-	if (item > 64) {
-		bug(40);
-		return;
-	}
+    if (item > 64) {
+        bug(40);
+        return;
+    }
 
-	p = adventtxt3[item - 1];
-	if (p == NULL)
-		bug(31);
-	else {
-		int c;
-		int n = state + 2;
+    p = adventtxt3[item - 1];
+    if (p == NULL) {
+        bug(31);
+    } else {
+        int c;
+        int n = state + 2;
 
-		while (n--) {
-			while ((c = *p++) != '/') {
-				if (c == '\0')
-					bug(32);
-			}
-		}
-		for (n = 0; p[n] != '\0' && p[n] != '/'; n++)
-			putchar(p[n]);
-	}
+        while (n--) {
+            while ((c = *p++) != '/') {
+                if (c == '\0') {
+                    bug(32);
+                }
+            }
+        }
+        for (n = 0; p[n] != '\0' && p[n] != '/'; n++)
+            putchar(p[n]);
+    }
 }
 
 /*
 	Print a long location description from "advent1.txt"
 */
-void desclg(int loc)
-{
-	fputs(adventtxt1[loc - 1], stdout);
+void desclg(int loc) {
+    fputs(adventtxt1[loc - 1], stdout);
 
 }
 
 /*
 	Print a short location description from "advent2.txt"
 */
-void descsh(int loc)
-{
-	fputs(adventtxt2[loc - 1], stdout);
+void descsh(int loc) {
+    fputs(adventtxt2[loc - 1], stdout);
 
 }
 
@@ -152,42 +153,45 @@ void descsh(int loc)
 	val  is the minimum acceptable value,
 		if != 0 return %1000
 */
-int vocab(char *word, int val)
-{
-	int v1, v2;
+int vocab(char * word, int val) {
+    int v1, v2;
 
-	if ((v1 = binary(word, wc, MAXWC)) >= 0) {
-		v2 = binary(word, wc, MAXWC - 1);
-		if (v2 < 0)
-			v2 = v1;
-		if (!val)
-			return wc[v1].acode < wc[v2].acode ? wc[v1].acode : wc[v2].acode;
-		if (val <= wc[v1].acode)
-			return wc[v1].acode % 1000;
-		else if (val <= wc[v2].acode)
-			return wc[v2].acode % 1000;
-		else
-			return -1;
-	} else
-		return -1;
+    if ((v1 = binary(word, wc, MAXWC)) >= 0) {
+        v2 = binary(word, wc, MAXWC - 1);
+        if (v2 < 0) {
+            v2 = v1;
+        }
+        if (!val) {
+            return wc[v1].acode < wc[v2].acode ? wc[v1].acode : wc[v2].acode;
+        }
+        if (val <= wc[v1].acode) {
+            return wc[v1].acode % 1000;
+        } else if (val <= wc[v2].acode) {
+            return wc[v2].acode % 1000;
+        } else {
+            return -1;
+        }
+    } else {
+        return -1;
+    }
 }
 
-int binary(char *w, struct wac wctable[], int maxwc)
-{
-	int lo, mid, hi, check;
+int binary(char * w, struct wac wctable[], int maxwc) {
+    int lo, mid, hi, check;
 
-	lo = 0;
-	hi = maxwc - 1;
-	while (lo <= hi) {
-		mid = (lo + hi) / 2;
-		if ((check = strcmp(w, wctable[mid].aword)) < 0)
-			hi = mid - 1;
-		else if (check > 0)
-			lo = mid + 1;
-		else
-			return mid;
-	}
-	return -1;
+    lo = 0;
+    hi = maxwc - 1;
+    while (lo <= hi) {
+        mid = (lo + hi) / 2;
+        if ((check = strcmp(w, wctable[mid].aword)) < 0) {
+            hi = mid - 1;
+        } else if (check > 0) {
+            lo = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
 }
 
 /*
@@ -197,109 +201,102 @@ int binary(char *w, struct wac wctable[], int maxwc)
 /*
 	Routine to test for darkness
 */
-int dark(void)
-{
-	return !(cond[loc] & LIGHT) && (!prop[LAMP] || !here(LAMP));
+int dark(void) {
+    return !(cond[loc] & LIGHT) && (!prop[LAMP] || !here(LAMP));
 }
 
 /*
 	Routine to tell if an item is present.
 */
-int here(int item)
-{
-	return place[item] == loc || toting(item);
+int here(int item) {
+    return place[item] == loc || toting(item);
 }
 
 /*
 	Routine to tell if an item is being carried.
 */
-int toting(int item)
-{
-	return place[item] == -1;
+int toting(int item) {
+    return place[item] == -1;
 }
 
 /*
 	Routine to tell if a location causes
 	a forced move.
 */
-int forced(int atloc)
-{
-	return cond[atloc] == 2;
+int forced(int atloc) {
+    return cond[atloc] == 2;
 }
 
 /*
 	Routine true x% of the time.
 */
-int pct(int x)
-{
-	return rand() % 100 < x;
+int pct(int x) {
+    return rand() % 100 < x;
 }
 
 /*
 	Routine to tell if player is on
 	either side of a two sided object.
 */
-int at(int item)
-{
-	return place[item] == loc || fixed[item] == loc;
+int at(int item) {
+    return place[item] == loc || fixed[item] == loc;
 }
 
 /*
 	Routine to destroy an object
 */
-void dstroy(int obj)
-{
-	move(obj, 0);
+void dstroy(int obj) {
+    move(obj, 0);
 }
 
 /*
 	Routine to move an object
 */
-void move(int obj, int where)
-{
-	int from;
+void move(int obj, int where) {
+    int from;
 
-	from = (obj < MAXOBJ) ? place[obj] : fixed[obj - MAXOBJ];
-	if (from > 0 && from <= 300)
-		carry(obj, from);
-	drop(obj, where);
+    from = (obj < MAXOBJ) ? place[obj] : fixed[obj - MAXOBJ];
+    if (from > 0 && from <= 300) {
+        carry(obj, from);
+    }
+    drop(obj, where);
 }
 
 /*
 	Juggle an object
 	currently a no-op
 */
-void juggle(int loc)
-{
-	(void)loc;
+void juggle(int loc) {
+    (void) loc;
 }
 
 /*
 	Routine to carry an object
 */
-void carry(int obj, int where)
-{
-	(void)where;
+void carry(int obj, int where) {
+    (void) where;
 
-	if (obj < MAXOBJ) {
-		if (place[obj] == -1)
-			return;
-		place[obj] = -1;
-		++holding;
-	}
+    if (obj < MAXOBJ) {
+        if (place[obj] == -1) {
+            return;
+        }
+        place[obj] = -1;
+        ++holding;
+    }
 }
 
 /*
 	Routine to drop an object
 */
-void drop(int obj, int where)
-{
-	if (obj < MAXOBJ) {
-		if (place[obj] == -1)
-			--holding;
-		place[obj] = where;
-	} else
-		fixed[obj - MAXOBJ] = where;
+void drop(int obj, int where) {
+    if (obj < MAXOBJ) {
+        if (place[obj] == -1) {
+            --holding;
+        }
+        place[obj] = where;
+    } else {
+        fixed[obj - MAXOBJ] = where;
+    }
 }
 
 /*
@@ -307,46 +304,45 @@ void drop(int obj, int where)
 	value used to set the negated prop values
 	for the repository.
 */
-int put(int obj, int where, int pval)
-{
-	move(obj, where);
-	return (-1) - pval;
+int put(int obj, int where, int pval) {
+    move(obj, where);
+    return (-1) - pval;
 }
+
 /*
 	Routine to check for presence
 	of dwarves..
 */
-int dcheck(void)
-{
-	int i;
+int dcheck(void) {
+    int i;
 
-	for (i = 1; i < (DWARFMAX - 1); ++i){
-		if (dloc[i] == loc)
-			return i;
-	}
-	return 0;
+    for (i = 1; i < (DWARFMAX - 1); ++i) {
+        if (dloc[i] == loc) {
+            return i;
+        }
+    }
+    return 0;
 }
 
 /*
 	Determine liquid in the bottle
 */
-int liq(void)
-{
-	int i, j;
-	i = prop[BOTTLE];
-	j = -1 - i;
-	return liq2(i > j ? i : j);
+int liq(void) {
+    int i, j;
+    i = prop[BOTTLE];
+    j = -1 - i;
+    return liq2(i > j ? i : j);
 }
 
 /*
 	Determine liquid at a location
 */
-int liqloc(int loc)
-{
-	if (cond[loc] & LIQUID)
-		return liq2(cond[loc] & WATOIL);
+int liqloc(int loc) {
+    if (cond[loc] & LIQUID) {
+        return liq2(cond[loc] & WATOIL);
+    }
 
-	return liq2(1);
+    return liq2(1);
 }
 
 /*
@@ -354,16 +350,14 @@ int liqloc(int loc)
 		 1 to nothing
 		 2 to OIL
 */
-int liq2(int pbottle)
-{
-	return (1 - pbottle) * WATER + (pbottle >> 1) * (WATER + OIL);
+int liq2(int pbottle) {
+    return (1 - pbottle) * WATER + (pbottle >> 1) * (WATER + OIL);
 }
 
 /*
 	Fatal error routine
 */
-void bug(int n)
-{
-	printf("Fatal error number %d\n", n);
-	exit(-1);
+void bug(int n) {
+    printf("Fatal error number %d\n", n);
+    exit(-1);
 }
